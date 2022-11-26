dc927ab7b1ad17919193cdb392cc358da43f2a48
Macbook <108225103+Kate-Vabishchevich@users.noreply.github.com>
2022-11-26 14:43:22 +0200

    add 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const createGallery = galleryItems.map(cardEl).join("");
+
+function cardEl({ preview, original, description }) {
+    return `
+ <li>
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+     </a>
+   </li>
+  `;
+};
+console.log(createGallery);
+
+galleryEl.insertAdjacentHTML('beforeend', createGallery);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
